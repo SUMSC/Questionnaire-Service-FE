756059569964dd1b4ab377afe09960b150a91a10
LionTao <taojiachun980831@163.com>
2019-04-02 22:05:18 +0800

    fix url bugs

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const main = ctx => {
     ctx.response.type = 'html';
-    ctx.response.body = fs.createReadStream('dist\\index.html');
+    ctx.response.body = fs.createReadStream('dist/index.html');
 };
 
 module.exports = {
